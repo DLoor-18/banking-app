687a65973ed3032bab85cde762e1675053aab32a
D-Loor <diego18.loor@gmail.com>
2025-01-21 19:15:37 -0500

    Login UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "login",
+        component: LoginComponent
+    },
+    {
+        path: "not-found",
+        component: NotFoundComponent
+    },
+    {
+        path: "**",
+        redirectTo: "not-found"
+    }
+];
