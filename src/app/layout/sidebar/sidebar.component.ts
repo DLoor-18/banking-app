f7eb2ee194349cd67ca854440d42c83b3feedf86
D-Loor <diego18.loor@gmail.com>
2025-01-23 01:53:27 -0500

    SideBar UI

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,7 +6,11 @@ import { Component } from '@angular/core';
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+  private collapseBtn: HTMLElement | null = null; 
+  private sidebar: HTMLElement | null = null; 
+  private menuLinks: HTMLElement | null = null; 
+  private collapsedClass = 'collapsed';
 
   menu = [
     {
@@ -49,5 +53,45 @@ export class SidebarComponent {
     }
   ];
   
+  constructor(private renderer: Renderer2, private el: ElementRef, private cdr: ChangeDetectorRef) {}
+  ngOnInit(): void {
+    this.collapseBtn = this.el.nativeElement.querySelector('.collapse-btn');  
+    this.sidebar = this.el.nativeElement.querySelector('.sidebar');   
+    this.menuLinks = this.el.nativeElement.querySelector('.admin-menu a');  
 
+    // for (const link of this.menuLinks) {
+    //   link.addEventListener("mouseenter", function () {
+    //     if (
+    //       this.sidebar.classList.contains(this.collapsedClass) &&
+    //       window.matchMedia("(min-width: 768px)").matches
+    //     ) {
+    //       const tooltip = this.querySelector("span").textContent;
+    //       this.setAttribute("title", tooltip);
+    //     } else {
+    //       this.removeAttribute("title");
+    //     }
+    //   });
+    // }
+
+  }
+  
+
+  toggleMenu(): void {
+    if(!this.collapseBtn) {
+      return;
+    }
+
+    if(this.sidebar?.classList.contains(this.collapsedClass)){
+        this.renderer.removeClass(this.sidebar, this.collapsedClass);
+    }else {
+        this.renderer.addClass(this.sidebar, this.collapsedClass);
+    }
+
+    const ariaExpanded = this.collapseBtn.getAttribute('aria-expanded') === 'true';
+    const ariaLabel = this.collapseBtn.getAttribute('aria-label');
+
+    this.renderer.setAttribute(this.collapseBtn, 'aria-expanded', String(!ariaExpanded));
+    this.renderer.setAttribute(this.collapseBtn, 'aria-label', ariaLabel === 'collapse menu' ? 'expand menu' : 'collapse menu');
+    this.cdr.detectChanges()
+  }
 }
